e718efa589fdb11d7e27982ea225ce6df3e0e51d
lizandrO <lisandrov719@gmail.com>
2026-08-15 15:25:12 -0600

    fix: update db_conexion.py

--- sqlite/db_conexion.py
+++ sqlite/db_conexion.py
@@ -3,5 +3,5 @@ import sqlite3
 import os
 
 def get_conexion():
-    ruta_db=os.path.join('/home/lizandro/Escritorio/loud/sqlite', 'loud_db.db')
+    ruta_db=os.path.join('../loud/sqlite', 'loud_db.db')
     return sqlite3.connect(ruta_db)
